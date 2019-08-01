788a27a258c82435d0950dc1c1c2d735e5f03e48
Tyler Nichols <tegulator98@gmail.com>
2019-07-31 20:05:04 -0600

    added swithces and started to code update.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer")
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -19,5 +20,85 @@ function queryAllItems() {
         console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | $" + res[i].price + " | " + res[i].stock_quantity);
       }
       console.log("-----------------------------------");
+      choice();
     });
   }
+  function choice(){
+  inquirer.prompt([
+    {
+    name: "product",
+    message: "What would you like to buy?",
+    type: "list",
+    choices: [
+      "Blue-Eyes White Dragon",
+      "Base Set Charizard 1st Edition",
+      "Liliana of the Veil",
+      "Dark Magician",
+      "Base Set Blastoise",
+      "Gideon Blackblade Mythic Foil",
+      "Red-Eyes Black Dragon Anniversary Pack",
+      "Base Set Venusaur",
+      "Jace, Vryn's Prodigy",
+      "Millenium Eyes Restrict"
+    ]
+    }.then(function(answer){
+      switch (answer.action) {
+        case "Blue-Eyes White Dragon":
+          artistSearch();
+          break;
+  
+        case "Base Set Charizard 1st Edition":
+          multiSearch();
+          break;
+  
+        case "Liliana of the Veil":
+          rangeSearch();
+          break;
+  
+        case "Dark Magician":
+          songSearch();
+          break;
+  
+        case "Base Set Blastoise":
+          songAndAlbumSearch();
+          break;
+        
+        case "Gideon Blackblade Mythic Foil":
+          songAndAlbumSearch();
+          break;
+        
+        case "Red-Eyes Black Dragon Anniversary Pack":
+          songAndAlbumSearch();
+          break;
+
+        case "Base Set Venusaur":
+            songAndAlbumSearch();
+            break;
+
+        case "Jace, Vryn's Prodigy":
+          songAndAlbumSearch();
+          break;
+
+        case "Millenium Eyes Restrict":
+          songAndAlbumSearch();
+          break;
+      }
+    })
+  ])
+}
+
+function purchase(){
+  inquirer.prompt([
+    {
+    name: "purchase",
+    message: "How many would you like to buy? (Numbers only)",
+    type: "number"
+    }.then(function(answer){
+      connection.query("UPDATE * FROM products", function(err) {
+
+        if (err) throw err;
+
+      })
+    })
+  ])
+}
