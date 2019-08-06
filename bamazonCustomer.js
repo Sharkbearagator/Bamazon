30281675ebd61bf5fd32e0fc9e3a2843e5b42329
Tyler Nichols <tegulator98@gmail.com>
2019-08-05 19:33:32 -0600

    Finished product, added screenshots

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -31,65 +31,71 @@ function queryAllItems() {
     }
     console.log("-----------------------------------");
     purchase();
-  });
-}
 
-function purchase() {
-  inquirer.prompt([
-    {
-      name: "product",
-      message: "What would you like to buy?",
-      type: "list",
-      choices: [
-        "Blue-Eyes White Dragon",
-        "Base Set Charizard 1st Edition",
-        "Liliana of the Veil",
-        "Dark Magician",
-        "Base Set Blastoise",
-        "Gideon Blackblade Mythic Foil",
-        "Red-Eyes Black Dragon Anniversary Pack",
-        "Base Set Venusaur",
-        "Jace, Vryn's Prodigy",
-        "Millenium Eyes Restrict"
-      ]
-    },
-  
-    {
-      name: "quantity",
-      type: "number",
-      message: "How many would you like?"
-    },
-  ])
-    .then(function(answer) {
-      connection.query("SELECT * FROM products", function(err, res2) {
-        if (err) throw err;
-        var item = answer.product;
-        console.log(item);
-        if (answer.quantity <= res2[item].stock_quantity) {
-          var newQuantity = (res2[item].stock_quantity - answer.quantity);
+    function purchase() {
+      inquirer
+        .prompt([
+          {
+            name: "product",
+            message: "What would you like to buy?",
+            type: "list",
+            choices: [
+              "Blue-Eyes White Dragon",
+              "Base Set Charizard 1st Edition",
+              "Liliana of the Veil",
+              "Dark Magician",
+              "Base Set Blastoise",
+              "Gideon Blackblade Mythic Foil",
+              "Red-Eyes Black Dragon Anniversary Pack",
+              "Base Set Venusaur",
+              "Jace, Vryn's Prodigy",
+              "Millenium Eyes Restrict"
+            ]
+          },
 
-          connection.query(
-            "UPDATE products SET ? WHERE ?",
-            [
-              {
-                stock_quantity: newQuantity
-              },
-              {
-                product_name: answer.product
-              }
-            ],
-            function(error) {
-              if (error) throw err;
-              console.log("Order Successful!");
-              var total = answer.quantity * res2[item].price;
-              console.log("The total is: $" + total);
+          {
+            name: "quantity",
+            type: "number",
+            message: "How many would you like?"
+          }
+        ])
+        .then(function(answer) {
+          var price = answer.product
+          connection.query("SELECT * FROM products", function(err, res) {
+            if (err) throw err;
+            var item = answer.product;
+            console.log(item);
+            if (answer.quantity <= res[0].stock_quantity) {
+              var newQuantity = res[0].stock_quantity - answer.quantity;
+              connection.query(
+                "SELECT stock_quantity, price FROM products WHERE product_name = ?",[price],
+                function(err, res) {
+                  if (err) throw err;
+                  console.log("Order Successful!");
+                  var total = res[0].price * answer.quantity;
+                  console.log(total);
+                  console.log("The total is: $" + total);
+
+                  connection.query(
+                    "UPDATE products SET ? WHERE ?",
+                    [
+                      {
+                        stock_quantity: newQuantity,
+                        product_name: answer.product
+                      }
+                    ],
+                    console.log("Product Updated!")
+                  );
+                }
+              );
+
+              connection.end();
+            } else {
+              console.log("Order Failed, Insufficient Quantity");
               connection.end();
             }
-          );
-        } else {
-          console.log("Order Failed, Insufficient Quantity");
-          connection.end();
-        }
-      });
-    });
-  }
+          });
+        });
+    }
+  });
+}
